15330f2e1eefe7db276c83b3a0ef37ee08385c0e
Jumana Obeid <color.ui>
2017-04-26 12:54:21 +0300

    gray all comments

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -28,11 +28,11 @@
 		<tbody>
 			@foreach ($completedTasks as $task)
 			  <tr>
-			    <td>{{$task->title}}</td>
-				<td>{{substr($task->notes,0,50)}}{{strlen($task->notes)>50?"...":""}}</td>
-				<td>{{$task->start_date}}</td>
-				<td>{{$task->due_date}}</td>
-				<td><a href="{{route('tasks.show',$task->id)}}" class="btn btn-default btn-sm">View</a><a href="{{route('tasks.edit',$task->id)}}" class="btn btn-default btn-sm">Edit</a></td>
+			    <td><s>{{$task->title}}</s></td>
+				<td><s>{{substr($task->notes,0,50)}}{{strlen($task->notes)>50?"...":""}}</s></td>
+				<td><s>{{$task->start_date}}</s></td>
+				<td><s>{{$task->due_date}}</s></td>
+				<td><s><a href="{{route('tasks.show',$task->id)}}" class="btn btn-default btn-sm">View</a><a href="{{route('tasks.edit',$task->id)}}" class="btn btn-default btn-sm">Edit</a></td>
 			  </tr>
 			@endforeach
 		</tbody>
