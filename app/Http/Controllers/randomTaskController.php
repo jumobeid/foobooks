74f5423681c7de5074752c11a5fd78ab57307e2c
Jumana Obeid <color.ui>
2017-04-28 13:56:56 +0300

    updated random

--- app/Http/Controllers/randomTaskController.php
+++ app/Http/Controllers/randomTaskController.php
@@ -9,21 +9,28 @@ class randomTaskController extends Controller
 {
     public function show()
 	{
+
+	  
 	 //create a variable that holds all the not completed tasks
 	 $notcompletedTasks= Task::where('status', '=', 'Not completed')->get();
-	 $rand = $notcompletedTasks->random();
-	
-
-	return view('rand.show')->with('rand',$rand);
+	 if(is_null($notcompletedTasks)) {
+            Session::flash('fail', 'All tasks are completed');
+			return redirect('/tasks');
+     }else{
+		   $rand = $notcompletedTasks->random();
+           return view('rand.show')->with('rand',$rand);
+	 } 
 	}
 	
 	public function edit()
 	{
 	 //create a variable that holds all the not completed tasks
 	 $notcompletedTasks= Task::where('status', '=', 'Not completed')->get();
-	 $rand = $notcompletedTasks->random();
-	
-
-	return view('rand.edit')->with('rand',$rand);
-	}
+	 if(is_null($notcompletedTasks)) {
+            Session::flash('fail', 'All tasks are completed');
+			return redirect('/tasks');
+     }else{
+		   $rand = $notcompletedTasks->random();
+           return view('rand.show')->with('rand',$rand);
+	 } 
 }
